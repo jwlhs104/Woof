2c9755fa219b54ae269ea77b891228c13a0a655f
jwlhs104 <johnny85215.cs08g@nctu.edu.tw>
2021-11-20 18:30:52 +0800

    Woof complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [show, setShow] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="WoofCount">{count}</div>
+        <div id="WoofClick" onClick={
+          () => 
+          {
+            setCount(count+1)
+            setShow(true)
+            console.log(show)
+          }
+        }>
+          <div 
+            className={show ? 'Woof' : 'Woof hide'}
+            onTransitionEnd={() => {
+              setShow(false)
+              console.log(show)
+            }
+            }
+          >
+            Woof!
+          </div>
+        </div>
       </header>
     </div>
   );
