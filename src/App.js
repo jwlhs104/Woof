478a07f7449b7c9b4af8830982b97cb5f9047132
jwlhs104 <johnny85215.cs08g@nctu.edu.tw>
2021-11-21 21:44:32 +0800

    Personal login finished

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import Dog from './components/pages/dog'
+import { Web3ReactProvider } from '@web3-react/core';
+import Web3 from 'web3';
+
+function getLibrary(provider){
+  return new Web3(provider)
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [show, setShow] = useState(false);
-  useEffect(()=>{
-    fetch('/leaderboard')
-    .then(response => response.json())
-    .then(data => {
-      setCount(data[0]['count'])
-    })
-  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <div id="WoofCount">{count}</div>
-        <div id="WoofClick" onClick={
-          () => 
-          {
-            setCount(count+1)
-            setShow(true)
-
-            const dog = {"name":"Dog1", "count":1};
-            console.log(JSON.stringify(dog))
-            fetch('/dog', {
-              method: 'POST',
-              headers: { "Content-Type": "text/json"},
-              body: JSON.stringify(dog)
-            })
-          }
-        }>
-          <div 
-            className={show ? 'Woof' : 'Woof hide'}
-            onTransitionEnd={() => {
-              setShow(false)
-            }
-            }
-          >
-            Woof!
-          </div>
-        </div>
-      </header>
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="App">
+        <header className="App-header">
+          <Dog/>
+        </header>
+      </div>
+    </Web3ReactProvider>
   );
 }
 
